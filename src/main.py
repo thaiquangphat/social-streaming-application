a4f8315d558e573c3685c6dc443b765597d571eb
Thái Quang Phát <quangphatthai@gmail.com>
2025-10-07 15:32:08 +0700

    add: sample data in main.py, docker compose, Dockerfile

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
 from ultis.spark import get_spark_session
 from processing.cleaner import clean_text_udf
-from processing.keyword_extractor import extract_keywords_udf
-from processing.embedder import embed_text_udf
+from processing.keyword_extractor import keyword_extractor_udf
+from processing.embedder import embedder_udf
 from pyspark.sql.functions import col
 
 def main():
     spark = get_spark_session("SparkPreprocessing")
 
-    df = spark.read.json("data/raw/sample.json")
+    df = spark.read.option("multiLine", True).json(r"/opt/spark-data/raw/sample.json")
+    df.printSchema()
 
     df = df.withColumn("clean_text", clean_text_udf(col("text")))
-    df = df.withColumn("keywords", extract_keywords_udf(col("clean_text")))
-    df = df.withColumn("embedding", embed_text_udf(col("clean_text")))
+    df = df.withColumn("keywords", keyword_extractor_udf(col("clean_text")))
+    df = df.withColumn("embedding", embedder_udf(col("clean_text")))
 
-    df.write.mode("overwrite").json("data/processed/")
+    df.write.mode("overwrite").json(r"/opt/spark-data/processed/")
     spark.stop()
 
 if __name__ == "__main__":
     main()
-    # hello
